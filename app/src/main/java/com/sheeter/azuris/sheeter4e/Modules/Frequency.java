02ab797e562de56872b1da46126b01726ac93a5c
Matt Buchanan <mbkodex42@hotmail.com>
2017-06-05 02:00:08 -0400

    I DON'T UNDERSTAND FUCK - PowersListViewAdapter

--- app/src/main/java/com/sheeter/azuris/sheeter4e/Modules/Frequency.java
+++ app/src/main/java/com/sheeter/azuris/sheeter4e/Modules/Frequency.java
@@ -4,7 +4,7 @@ package com.sheeter.azuris.sheeter4e.Modules;
  * Created by Azuris on 2017-06-05.
  */
 
-enum Frequency {
+public enum Frequency {
     AT_WILL,
     ENCOUNTER,
     DAILY
--- app/src/main/java/com/sheeter/azuris/sheeter4e/PowersFragment.java
+++ app/src/main/java/com/sheeter/azuris/sheeter4e/PowersFragment.java
@@ -5,6 +5,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+import com.sheeter.azuris.sheeter4e.Modules.Frequency;
+import com.sheeter.azuris.sheeter4e.Modules.Power;
 
 /**
  * Created by Azuris on 2017-06-03.
@@ -15,6 +19,16 @@ public class PowersFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.content_powers, container, false);
+        View root =  inflater.inflate(R.layout.content_powers, container, false);
+
+        ListView listView = (ListView) root.findViewById(R.id.Powers_List);
+        PowersListViewAdapter adapter = new PowersListViewAdapter(getActivity());
+
+        listView.setAdapter(adapter);
+        adapter.add(new Power("Magic Missle", Frequency.AT_WILL));
+        adapter.notifyDataSetChanged();
+
+
+        return root;
     }
 }
